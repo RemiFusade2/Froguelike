064cb2c6d68e72993c8a03919c349a57d04adab3
Remi Fusade <remi.fusade@gmail.com>
2022-11-12 13:40:05 +0100

    Bug fix probably
    Signed-off-by: Remi Fusade <remi.fusade@gmail.com>

--- Halloween Game Jam/Assets/Scripts/UI/UIManager.cs
+++ Halloween Game Jam/Assets/Scripts/UI/UIManager.cs
@@ -383,6 +383,7 @@ public class UIManager : MonoBehaviour
     public void ShowPauseScreen()
     {
         musicManager.PauseMusic();
+        pausePanel.SetActive(true);
         pausePanelAnimator.SetBool("Visible", true);
     }
 
