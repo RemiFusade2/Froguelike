615e034cf2bfa65b3d85bf2f9e7c68dd469461ce
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2024-09-13 05:02:25 +0300

    I'm not even sure any of this made a difference

--- Froguelike/Assets/Scripts/Settings/MusicChoiceButtonBehaviour.cs
+++ Froguelike/Assets/Scripts/Settings/MusicChoiceButtonBehaviour.cs
@@ -9,9 +9,15 @@ public class MusicChoiceButtonBehaviour : Button
     public Button yesButton;
     public Button noButton;
 
+    private bool transitioning = false;
+
     public override void OnPointerEnter(PointerEventData eventData)
     {
-        base.OnPointerEnter(eventData);
+        if (eventData.delta.x != 0 || eventData.delta.y != 0)
+        {
+            base.OnPointerEnter(eventData);
+        }
+
         if (interactable)
         {
             Select();
@@ -30,5 +36,18 @@ public class MusicChoiceButtonBehaviour : Button
     public void SetBoolInteractable()
     {
         GetComponent<Animator>().SetBool("Interactable", interactable);
+    }
+
+    public override void Select()
+    {
+        if (!transitioning)
+        {
+            base.Select();
+            transitioning = true;
+        }
+        else
+        {
+            transitioning = false;
+        }
     }
 }
